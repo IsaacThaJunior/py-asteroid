4353c081594f38f7b3f8b0b535ee4597b55246c7
Isaac Okoro <okoro669@gmail.com>
2025-12-11 10:12:51 +0100

    getting the shapes to fly across the screen

--- main.py
+++ main.py
@@ -1,21 +1,35 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
 
-pygame.init()
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    new_time = pygame.time.Clock()
+    dt = 0
+
+    drawable = pygame.sprite.Group()
+    updatable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        
-        screen.fill("black")
-        pygame.display.flip()
-        
 
+        screen.fill("black")
+        player.draw(screen)
+        player.update(dt)
+        pygame.display.flip()
+
+        new_time.tick(60)
+        dt = new_time.get_time() / 1000  # Convert to seconds
 
 
 if __name__ == "__main__":
